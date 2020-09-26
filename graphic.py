ca2364147c307037515cd5c1bdc9cfa39b91e284
Ryan Nguyen <ryan.q.nguyen@gmail.com>
2020-09-26 16:59:40 -0600

    Add files via upload

---
+++ graphic.py
@@ -0,0 +1,17 @@
+from graphics import *
+
+def main():
+    win = GraphWin("My Window", 500, 500)
+    win.setBackground(color_rgb(0, 0, 0))
+    pt = Point (250,250)
+    cir = Circle(pt, 50)
+    cir.setFill(color_rgb(100,255,50))
+    cir.draw(win)
+    pt = Point (300,300)
+    cir = Circle(pt, 50)
+    cir.setFill(color_rgb(0, 0, 255))
+    cir.draw(win)
+
+    win.getMouse()
+    win.close()
+main()
